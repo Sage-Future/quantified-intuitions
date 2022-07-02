32c35765ecb28a37bfd95217bca257a1c57d879b
Platinuman <AaronHo1998@gmail.com>
2022-07-02 18:31:27 -0400

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Pastcasting</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
