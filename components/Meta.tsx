3c52f2fcafd928cfed923cf9b4440fed01fa2638
Platinuman <AaronHo1998@gmail.com>
2022-08-11 23:51:12 -0400

    meta stuff

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -3,19 +3,19 @@ import { NextSeo } from "next-seo";
 export const Meta = () => {
   return (
     <NextSeo
-      title="Pastcast"
-      description="Pastcast is a tool for practicing forecasting."
-      canonical="https://pastcast-sage.vercel.app/"
+      title="Pastcasting"
+      description="Pastcasting is a tool for practicing forecasting."
+      canonical="https://www.pastcasting.com/"
       openGraph={{
         type: "website",
         locale: "en_US",
-        url: "https://pastcast-sage.vercel.app/",
-        title: "Pastcast",
-        description: "Pastcast is a tool for practicing forecasting.",
-        site_name: "Pastcast",
+        url: "https://www.pastcasting.com/",
+        title: "Pastcasting",
+        description: "Pastcasting is a tool for practicing forecasting.",
+        site_name: "Pastcasting",
         images: [
           {
-            url: "https://pastcast-sage.vercel.app/og.png",
+            url: "https://www.pastcasting.com/og.png",
             width: 1024,
             height: 1024,
           },
