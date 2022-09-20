baf44f16e8c883e30689196f9edb9216eb4009cf
Platinuman <AaronHo1998@gmail.com>
2022-09-20 11:53:11 -0700

    add q

--- pages/calibration/faq.tsx
+++ pages/calibration/faq.tsx
@@ -19,6 +19,10 @@ const Faq = () => {
         "Someone is well-calibrated if the things they predict with X% chance of happening in fact occur X% of the time. ",
     },
     {
+      question: "How does the scoring system work?",
+      answer: "Interval scoring uses the formula developed by [this paper](https://arxiv.org/abs/1808.07501)."
+    }
+    {
       question: "I found an issue with one of the questions!",
       answer:
         "Please leave a comment at our [airtable](https://airtable.com/invite/l?inviteId=inv38Ivl9dVyg6i5H&inviteToken=9e9063ebbeabecfce042fa8960cad2906272c887ade48f469ae05d3cdaf63969&utm_medium=email&utm_source=product_team&utm_content=transactional-alerts) or email us at aaron@sage-future.org",
