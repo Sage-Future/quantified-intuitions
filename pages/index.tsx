5721fc487cb4e780ceb28837a1b97cc56214f49f
Adam Binks <adam.r.binks@gmail.com>
2023-07-07 14:34:37 +0100

    Add fatebook.io to apps

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,11 +30,18 @@ export const apps = [
     icon: <PlayIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />,
   },
   {
+    name: "Fatebook",
+    description: "The fastest way to track your predictions",
+    href: "https://fatebook.io",
+    icon: <PlayIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />,
+    banner: "New",
+    verb: "Add to Slack" 
+  },
+  {
     name: "Fatebook for Slack",
     description: "Track your predictions, right where your team works",
     href: "https://fatebook.io/for-slack",
     icon: <BoltIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />,
-    banner: "New",
     verb: "Add to Slack" 
   },
 ]
