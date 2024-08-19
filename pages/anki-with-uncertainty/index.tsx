8f811d7616131eb3673d36f0343d06cf5d6f680c
Adam Binksmith <adam.r.binks@gmail.com>
2024-08-19 09:44:25 +0100

    add link to Julius's Anki Calibrate

--- pages/anki-with-uncertainty/index.tsx
+++ pages/anki-with-uncertainty/index.tsx
@@ -10,7 +10,10 @@ import { NavbarGeneric } from "../../components/NavbarGeneric"
 const AnkiPage = () => {
   return (
     <div className="flex flex-col min-h-screen ">
-      <NextSeo title="Anki with Uncertainty" description="Turn any flashcard deck into a calibration training tool" />
+      <NextSeo
+        title="Anki with Uncertainty"
+        description="Turn any flashcard deck into a calibration training tool"
+      />
       <NavbarGeneric />
       <div className="bg-gray-50 grow">
         <div className="px-4 pt-12 lg:pt-16 mx-auto max-w-6xl">
@@ -18,57 +21,145 @@ const AnkiPage = () => {
             <h2 className="text-3xl mb-2 font-extrabold text-gray-900">
               Anki with Uncertainty
             </h2>
-            <h3 className="text-gray-600">Turn any flashcard deck into a calibration training tool</h3>
+            <h3 className="text-gray-600">
+              Turn any flashcard deck into a calibration training tool
+            </h3>
 
-            <Image src={"/anki_front.png"} alt="An Anki card that lets you input a range" width={700} height={595} />
+            <Image
+              src={"/anki_front.png"}
+              alt="An Anki card that lets you input a range"
+              width={700}
+              height={595}
+            />
 
-            <p>{"Instead of the exact answer, enter your 90% confidence interval - an interval that you're 90% confident the answer lies within."}</p>
+            <p>
+              {
+                "Instead of the exact answer, enter your 90% confidence interval - an interval that you're 90% confident the answer lies within."
+              }
+            </p>
 
-            <Image src={"/anki_back.png"} alt="The back of the card shows your score" width={700} height={595} />
+            <Image
+              src={"/anki_back.png"}
+              alt="The back of the card shows your score"
+              width={700}
+              height={595}
+            />
 
-            <p>{"Earn more points for a narrower correct interval, lose more points for a narrower incorrect interval."}</p>
+            <p>
+              {
+                "Earn more points for a narrower correct interval, lose more points for a narrower incorrect interval."
+              }
+            </p>
 
-            <p>{"Based on your score, we recommend you press Hard, Good or Easy."}</p>
+            <p>
+              {
+                "Based on your score, we recommend you press Hard, Good or Easy."
+              }
+            </p>
 
-            <p>{"Higher scores  = longer before you need to review the card again!"}</p>
+            <p>
+              {
+                "Higher scores  = longer before you need to review the card again!"
+              }
+            </p>
 
             <div className="flex justify-center">
-              <Image src={"/anki_scores.png"} alt="See your Anki calibration scores" width={345} height={700} />
+              <Image
+                src={"/anki_scores.png"}
+                alt="See your Anki calibration scores"
+                width={345}
+                height={700}
+              />
             </div>
 
-            <p>{"Track your calibration: press Tools → Show calibration scores. Perfect calibration = your X% confidence interval is correct X% of the time."}</p>
+            <p>
+              {
+                "Track your calibration: press Tools → Show calibration scores. Perfect calibration = your X% confidence interval is correct X% of the time."
+              }
+            </p>
 
             <h3 className="text-lg font-semibold mt-12">How to use</h3>
 
-            <p>{"1. Open Anki, go to Tools → Addons → Get addons, and paste this code:"} <b>694813595</b></p>
+            <p>
+              {
+                "1. Open Anki, go to Tools → Addons → Get addons, and paste this code:"
+              }{" "}
+              <b>694813595</b>
+            </p>
 
-            <p>{"2."} <Link href="Anki_with_uncertainty__example_deck.apkg">{"Download and open the example deck"}</Link>{" to add the Interval card type"}</p>
+            <p>
+              {"2."}{" "}
+              <Link href="Anki_with_uncertainty__example_deck.apkg">
+                {"Download and open the example deck"}
+              </Link>
+              {" to add the Interval card type"}
+            </p>
 
             <p>{"3. Add some Interval cards:"}</p>
 
             <div className="flex justify-center">
-              <Image src={"/anki_add_card.png"} alt="How to add an Anki interval card" width={345} height={440} />
+              <Image
+                src={"/anki_add_card.png"}
+                alt="How to add an Anki interval card"
+                width={345}
+                height={440}
+              />
             </div>
 
-            <p>You can rate this plugin on <Link href={"https://ankiweb.net/shared/info/694813595"}>AnkiWeb</Link>.</p>
+            <p>
+              You can rate this plugin on{" "}
+              <Link href={"https://ankiweb.net/shared/info/694813595"}>
+                AnkiWeb
+              </Link>
+              .
+            </p>
 
-            <p>The source code is on <Link href={"https://github.com/Sage-Future/anki-with-uncertainty"}>GitHub</Link>.</p>
-            
-            <p>{"We'd love your feedback at"} <a href="mailto:hello@sage-future.org">hello@sage-future.org</a> or on <Link href="https://discord.gg/mt9YVB8VDE">Discord</Link>.</p>
+            <p>
+              The source code is on{" "}
+              <Link
+                href={"https://github.com/Sage-Future/anki-with-uncertainty"}
+              >
+                GitHub
+              </Link>
+              .
+            </p>
 
-            <h3 className="text-lg font-semibold mt-12">More tools from Quantified Intuitions</h3>
+            <p>
+              {"We'd love your feedback at"}{" "}
+              <a href="mailto:hello@sage-future.org">hello@sage-future.org</a>{" "}
+              or on <Link href="https://discord.gg/mt9YVB8VDE">Discord</Link>.
+            </p>
+
+            <p>
+              See also{" "}
+              <Link href="https://github.com/JulHeg/AnkiCalibrateAddon">
+                Julius Hege&apos;s Anki Calibrate plugin
+              </Link>
+              .
+            </p>
+
+            <h3 className="text-lg font-semibold mt-12">
+              More tools from Quantified Intuitions
+            </h3>
             <div className="flex flex-wrap gap-2 py-4">
-              {apps.filter(app => app.name !== "Anki with Uncertainty").map(app => <AppCard key={app.name} app={app} />)}
+              {apps
+                .filter((app) => app.name !== "Anki with Uncertainty")
+                .map((app) => (
+                  <AppCard key={app.name} app={app} />
+                ))}
             </div>
           </div>
 
           <div className="max-w-xs mt-12 mb-6 m-auto">
-            <MailingListSignup buttonText="Subscribe to hear about our next tool" tags={["homepage"]} />
+            <MailingListSignup
+              buttonText="Subscribe to hear about our next tool"
+              tags={["homepage"]}
+            />
           </div>
         </div>
       </div>
       <Footer />
-    </div >
+    </div>
   )
 }
 
