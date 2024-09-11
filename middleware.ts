881db995a972f768306bd8938b483b48e4bf94e9
Adam Binksmith <adam.r.binks@gmail.com>
2024-09-11 10:18:16 +0100

    disable auth req on email API

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
 export { default } from "next-auth/middleware"
 // Require login on matching URLs:
-export const config = { matcher: [
-  "/calibration",
-  "/calibration/charts",
-  "/((?!estimation-game/for-organisers)estimation-game/.+)", // estimation-game/anything, except for estimation-game/for-organisers
-  "/pastcasting/charts",
-  "/pastcasting/settings",
-  "/pastcasting/multiplayer/:page*",
-] }
+export const config = {
+  matcher: [
+    "/calibration",
+    "/calibration/charts",
+    "/((?!estimation-game/for-organisers)estimation-game/.+)", // estimation-game/anything, except for estimation-game/for-organisers
+    "/pastcasting/charts",
+    "/pastcasting/settings",
+    "/pastcasting/multiplayer/:page*",
+    "/api/email/*",
+  ],
+}
